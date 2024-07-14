d57522da534f7bb00a46355ea8717109916a00ef
Santiago Fernandez <santif@gmail.com>
2024-07-13 21:02:53 -0300

    feat: Add new page for videocall functionality

---
+++ frontend/pages/call.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+import { ref } from 'vue'
+
+const startCall = () => {
+    console.log('Call started!')
+}
+</script>
+
+<template>
+  <div>
+    <h1>Videocall</h1>
+    <button variant="destructive" @click="startCall">Start Call</button>
+  </div>
+</template>
